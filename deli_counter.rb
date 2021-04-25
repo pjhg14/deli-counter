03ace3c06c0f1881b65c856244079f9a0dd338c3
pj <pjhg14@gmail.com>
2021-04-25 18:02:56 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def line(deli)
+    if deli.size <= 0
+        puts "The line is currently empty."
+        return 
+    end
+
+    line_array = deli.map.with_index do |name, index|
+        "#{index + 1}. #{name}"
+    end
+
+    line_string = line_array.join(" ")
+
+    puts "The line is currently: " + line_string
+end
+
+def take_a_number(deli, person)
+    deli << person
+    puts "Welcome, #{person}. You are number #{deli.size} in line."
+end
+
+def now_serving(deli)
+    if deli.size <= 0
+        puts "There is nobody waiting to be served!"
+        return
+    end
+
+    puts "Currently serving #{deli.shift}."
+end
